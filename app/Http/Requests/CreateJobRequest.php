e06c56e82deaab9ef619ca712c3fcdc81b9ac574
mk3390 <m.k@textat.tk>
2022-03-06 17:39:58 +0530

    API validator

--- app/Http/Requests/CreateJobRequest.php
+++ app/Http/Requests/CreateJobRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
 
 class CreateJobRequest extends FormRequest
 {

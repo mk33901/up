87c920970ebab8e929bef229828d77698710175c
mk3390 <m.k@textat.tk>
2022-05-22 09:30:02 +0530

    User Education

--- app/Models/UserEducation.php
+++ app/Models/UserEducation.php
@@ -2,20 +2,21 @@
 
 namespace App\Models;
 
+use App\Traits\UuidTraits;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserEducation extends Model
 {
-    use HasFactory;
+    use HasFactory,UuidTraits;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['uuid', 'user_id', 'from', 'to', 'education_id', 'study_id', 'description'];
+    protected $fillable = ['uuid','school', 'user_id', 'from', 'to', 'education_id', 'study_id', 'description'];
 
     /**
      * Get the user that owns the UserEducation
